e673ef555748663cebcd647d4d9e92b8d0152a8d
Никита Лясковский <124239663+niki6ka@users.noreply.github.com>
2023-03-04 16:00:36 +0300

    Add files via upload

---
+++ task47.cs
@@ -0,0 +1,25 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//////m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+Console.WriteLine("Please enter number of array rows:");
+    int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter number of array columns:");
+    int col = Convert.ToInt32(Console.ReadLine());
+double[,] TwoDimArray = new double[row, col];
+void FillTwoDimArray(double[,] TwoDimArray)
+{
+    for (int i = 0; i < TwoDimArray.GetLongLength(0); i++)
+        {
+            Console.WriteLine();
+                for (int j=0; j<TwoDimArray.GetLength(1); j++)
+                    {
+                        TwoDimArray[i, j] = new Random().NextDouble();
+                        Console.Write(TwoDimArray[i, j] + " ");
+                    }
+
+        }
+
+}
+FillTwoDimArray(TwoDimArray);
---
+++ task50.cs
@@ -0,0 +1,29 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента 
+// или же указание, что такого элемента нет.
+
+int r = new Random().Next(0, 10);
+int c = new Random().Next(0, 10);
+int[,] TwoDimArray = new int[r, c];
+void FillTwoDimArray(int[,] TwoDimArray)
+{
+    for (int i = 0; i < TwoDimArray.GetLongLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < TwoDimArray.GetLength(1); j++)
+        {
+            TwoDimArray[i, j] = new Random().Next(0, 10);
+            Console.Write(TwoDimArray[i, j] + " ");
+           
+        }
+         }
+}
+
+FillTwoDimArray(TwoDimArray);
+Console.WriteLine();
+Console.WriteLine("Please enter number of array rows (from 0):");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter number of array columns (from 0):");
+int col = Convert.ToInt32(Console.ReadLine());
+if (row > r || col > c) 
+{Console.WriteLine("There is no such element in the matrix!!!");}
+Console.WriteLine("Array element value with position:" + (row) + " " + (col) + " - " + TwoDimArray[row, col]);
